6c83cae4962f9b637084265dc0a52ed0505c9c5f
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-06-27 17:54:14 +0300

    add components

--- src/App/App.jsx
+++
@@ -1,9 +0,0 @@
-// import { Formik } from "formik";
-// import * as Yup from "yup";
-// import { ErrorMessage } from "formik";
-
-
-
-const App = () => { };
-
-export default App;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+// import { Formik } from "formik";
+// import * as Yup from "yup";
+// import { ErrorMessage } from "formik";
+// import ContactForm from "./ContactForm/ContactForm";
+// import ContactList from "./ContactList/ContactList";
+// import SearchBox from "./SearchBox/SearchBox";
+
+
+
+const App = () => {
+    return (
+    <div>
+  <h1>Phonebook</h1>
+  {/* <ContactForm />
+  <SearchBox />
+  <ContactList /> */}
+</div>
+    )
+
+
+ };
+
+export default App;
