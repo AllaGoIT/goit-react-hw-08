a237fbb441f2b342e31b3e3eae2fd01385b2ab3f
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-06-30 18:33:43 +0300

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,11 @@
-const Contact = ({data:{id,name,number}, onDelete}) => {
+import { FaUser } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa6";
+
+const Contact = ({ data: { id, name, number }, onDelete }) => {
     return (
         <div>
-            <p>{name}</p>
-            <p>{number}</p>
+            <p> <FaUser/> {name}</p>
+            <p> <FaPhone/> {number}</p>
             <button onClick= {()=>{onDelete(id)}}>Delete</button>
         </div>
     )
