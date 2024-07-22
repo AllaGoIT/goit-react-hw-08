3adc8f12013154464fb676b3da90f5413d27f8f9
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-07-22 12:16:31 +0300

    add code install libs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-const initionalState = {
-  balance: {
-    value: 0,
-  },
-};
-const rootReducer = (state = initionalState, action) => {
-  switch (action.type) {
+import { persistStore, persistReduser } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-    case "type action dispatch": {
-      return {
-        balance: {
-          value: state.balance.value + action.payload // from dispatch actions
-        }
-    }
-      } 
-    }
-    default:
-      return state;
-  }
+const persistConfig = {
+  key: "root", // balance
+  storage,
 };
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const persistor = persistStore(store);
+
+// const initionalState = {
+//   balance: {
+//     value: 0,
+//   },
+// };
+// const rootReducer = (state = initionalState, action) => {
+//   switch (action.type) {
+
+//     case "type action dispatch": {
+//       return {
+//         balance: {
+//           value: state.balance.value + action.payload // from dispatch actions
+//         }
+//     }
+//       }
+//     }
+//     default:
+//       return state;
+//   }
+// };
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer, //balance: persistedReducer,
 });
