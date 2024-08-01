bd2b0384743b2b352746e291d6b283445c9bc427
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-01 13:19:12 +0300

    add reducers code

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, deleteContact } from "./contactsOps";
 
-export const contactsSlice = createSlice({
+const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
     items: [],
     loading: false,
     error: null,
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchContacts.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchContacts.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      })
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        state.loading = false;
+      })
+      .addCase(addContact.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(deleteContact.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items.filter((item) => item.id !== action.payload.id);
+      })
+      .addCase(deleteContact.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      }),
 });
-
 // items: [
 //     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
 //     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
@@ -31,3 +69,5 @@ export const contactsSlice = createSlice({
 // export const contactsReducer = contactsSlice.reducer;
 
 // export const selectContacts = (state) => state.contacts.items;
+
+export default contactsSlice.reducer;
