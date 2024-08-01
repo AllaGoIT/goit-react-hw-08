2318fdd7cfbf1b4b5d15ef85afc1fff14931b7c0
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-01 10:38:57 +0300

    add start code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { contactsReducer } from "./contactsSlice.js";
-import { filterReducer } from "./filtersSlice.js";
+// import { configureStore } from "@reduxjs/toolkit";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// // } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import { contactsReducer } from "./contactsSlice.js";
+// import { filterReducer } from "./filtersSlice.js";
 
-const persistConfig = {
-  key: "contacts",
-  storage,
-  whitelist: ["items"],
-};
-const persistContactsReducer = persistReducer(persistConfig, contactsReducer);
+// const persistConfig = {
+//   key: "contacts",
+//   storage,
+//   whitelist: ["items"],
+// };
+// const persistContactsReducer = persistReducer(persistConfig, contactsReducer);
 
-export const store = configureStore({
-  reducer: {
-    contacts: persistContactsReducer,
-    filters: filterReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistContactsReducer,
+//     filters: filterReducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
