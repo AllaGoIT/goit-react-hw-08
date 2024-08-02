366adc9526a6a684d673fb6bfe702fe0789e287c
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-08-02 19:02:57 +0300

    add code else 1

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,16 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { register, login, logout, refresh } from "./operations";
 const authSlice = createSlice({
-    {
+  name: "users",
+  initialState: {
     user: {
-        name: null,
-        email: null,
+      name: null,
+      email: null,
     },
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
-},
-    extraReducers: (builder) => builder.addCase(()=>{}),
+  },
+  extraReducers: (builder) =>
+      builder.addCase((register.pending, (state) => {
+          state.user.name = true;
+          state.user.email = true;
+          state.token = true;
+      })).addCase((register.fulfilled, (state) => {
+          state.token = true;
+      })).addCase((register.rejected, (state) => {
+          state.user.name = null;
+          state.user.email = null;
+          state.token = null,
+          state.isLoggedIn= false,
+          state.isRefreshing= false,
+
+    })).addCase((login.pending, (state) => {
+          state.user.name = true;
+          state.user.email = true;
+          state.token = true;
+    })).addCase((login.fulfilled, (state) => {
+           state.token = true;
+          state.isLoggedIn = true;
+    })).addCase((login.rejected, (state) => {
+        state.user.name = null;
+          state.user.email = null;
+          state.token = null,
+          state.isLoggedIn= false,
+          state.isRefreshing= false,
+
+    }))
 });
 
 export default authSlice.reducer;
